348cb4a16aafda859dd797abeda0c1b20a383fd5
Mateusz Mańczak <mateuszmanczak@icloud.com>
2024-10-16 15:12:49 +0200

    Setup typescript

---
+++ build/app.js
@@ -0,0 +1,4 @@
+var canvas = document.querySelector('#canvas');
+canvas.style.backgroundColor = '#ddd';
+var ctx = canvas.getContext('2d');
+//# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,4 @@
+const canvas = document.querySelector('#canvas') as HTMLCanvasElement;
+canvas.style.backgroundColor = '#ddd'
+
+const ctx = canvas.getContext('2d')

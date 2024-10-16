260340181747b6a4a5a1ae38ddf4786d338ec1d8
Mateusz Mańczak <mateuszmanczak@icloud.com>
2024-10-16 15:20:17 +0200

    Test canvas stroke

--- build/app.js
+++ build/app.js
@@ -1,4 +1,10 @@
 var canvas = document.querySelector('#canvas');
 canvas.style.backgroundColor = '#ddd';
 var ctx = canvas.getContext('2d');
+ctx.clearRect(0, 0, 500, 500);
+ctx.strokeStyle = '#222';
+ctx.fillStyle = '#444';
+ctx.moveTo(100, 100);
+ctx.lineTo(200, 200);
+ctx.stroke();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,15 @@
 const canvas = document.querySelector('#canvas') as HTMLCanvasElement;
-canvas.style.backgroundColor = '#ddd'
+canvas.style.backgroundColor = '#ddd';
 
-const ctx = canvas.getContext('2d')
+const ctx = canvas.getContext('2d');
+
+// Reset canvas state
+ctx.clearRect(0, 0, 500, 500);
+
+// Setup colors
+ctx.strokeStyle = '#222';
+ctx.fillStyle = '#444';
+
+ctx.moveTo(100, 100);
+ctx.lineTo(200, 200);
+ctx.stroke();
